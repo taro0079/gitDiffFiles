08c6dd2c160fcdcf0ad9b2fe7e3f4ad261bf3dae
Taro Morita <morita0079@gmail.com>
2022-07-11 20:37:03 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+fn main() {
+    // let output = Command::new("ls")
+    //     .output()
+    //     .expect("failed to execute process");
+    // let stdout = String::from_utf8(output.stdout).unwrap();
+    // println!("{}", stdout);
+    // test
+    let out = compare("develop", "master");
+    println!("{}", out);
+}
+
+fn compare(origin: &str, to: &str) -> String {
+    let origin = origin;
+    let to = to;
+    let output = Command::new("git")
+        .args(["diff", "--name-only", origin, to])
+        .output()
+        .expect("error");
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    return stdout;
+}
